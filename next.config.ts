4f800fda3e2f1cd04ffbe38324ed6c86958081c5
jettyramanjaneyulu-dev <jettyramanjaneyulu@gmail.com>
2026-03-08 08:17:22 +0530

    clean hostinger deploy

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-
-  // important for Hostinger Node deployment
   output: "standalone",
-
   images: {
     unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "media.istockphoto.com",
-      },
-      {
-        protocol: "https",
-        hostname: "t4.ftcdn.net",
-      },
-      {
-        protocol: "https",
-        hostname: "plus.unsplash.com",
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "media.istockphoto.com" },
+      { protocol: "https", hostname: "t4.ftcdn.net" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
     ],
   },
 };
