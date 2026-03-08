f14945f94b787cc6e9bc45202945065d16fdb8da
jettyramanjaneyulu-dev <jettyramanjaneyulu@gmail.com>
2026-03-08 22:37:58 +0530

    fix:ramji

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
-import Banner from "./home/Banner";
-import Pharmaceuticals from "./home/Pharmaceuticals";
-import GlobalReach from "./home/GlobalReach";
-import Manufacturing from "./home/Manufacturing";
-import WhatWeDo from "./home/WhatWeDo";
-import CoreCapabilities from "./home/CoreCapabilities";
-import Testimonials from "./home/Testimonials";
+// import Banner from "./home/Banner";
+// import Pharmaceuticals from "./home/Pharmaceuticals";
+// import GlobalReach from "./home/GlobalReach";
+// import Manufacturing from "./home/Manufacturing";
+// import WhatWeDo from "./home/WhatWeDo";
+// import CoreCapabilities from "./home/CoreCapabilities";
+// import Testimonials from "./home/Testimonials";
 
 export default function HomePage() {
   return (
     <main className="relative">
-      <Banner />
+      <h1>ramji</h1>
+      {/* <Banner />
       <Pharmaceuticals />
       <GlobalReach />
       <Manufacturing />
       <WhatWeDo />
       <CoreCapabilities />
-      <Testimonials />
+      <Testimonials /> */}
     </main>
   );
 }
