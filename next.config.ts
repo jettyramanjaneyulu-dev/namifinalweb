dddfd4ee089da7b3508c19f983b1188abfe607d9
jettyramanjaneyulu-dev <jettyramanjaneyulu@gmail.com>
2026-03-08 09:10:16 +0530

    fix assets routing

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
   output: "standalone",
+
   images: {
     unoptimized: true,
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "media.istockphoto.com" },
-      { protocol: "https", hostname: "t4.ftcdn.net" },
-      { protocol: "https", hostname: "plus.unsplash.com" },
-    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/assets/:path*",
+        destination: "/public/assets/:path*",
+      },
+    ];
   },
 };
 
